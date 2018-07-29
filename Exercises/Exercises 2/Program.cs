a38101ea7f2e114386d2b8287b0ae09e9290626a
Sefton Munro <30001654@student.toiohomai.ac.nz>
2018-07-30 11:50:47 +1200

    1

---
+++ Exercises/Exercises 2/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Math;
+
+namespace Exercises_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string menu;
+
+            do
+            {
+                Console.WriteLine("'a' Input Circle Values");
+                Console.WriteLine("'b' Calculate Circle Area");
+                Console.WriteLine("'c' Input Square Values");
+                Console.WriteLine("'d' Calculate Square Area");
+                Console.WriteLine("'e' Exits the program");
+
+                menu = Console.ReadLine();
+                switch (menu)
+                {
+                    case "a":
+                        Math.GetCircleValues();
+                        break;
+                    case "b":
+                        Math.CalcCircleArea();
+                        break;
+                    case "c":
+                        Math.GetSquareValues();
+                        break;
+                    case "d":
+                        Math.CalcSquareArea();
+                        break;
+                    case "e":
+                        break;
+                    default:
+                        Console.WriteLine("Error. Not an option!");
+                        break;
+
+                }
+            } while (menu != "e");
+        }
+    }
+
+    class Math
+    {
+        public static int GetCircleValues()
+        {
+            Console.WriteLine("Enter the circles radius:");
+            return int.Parse(Console.ReadLine());
+        }
+
+        public static void CalcCircleArea(int rad)
+        {
+            return Math.Pow(3.14 * rad, 2.0);
+        }
+
+        public static int GetSquareValues(int w, int h)
+        {
+            Console.WriteLine("Enter the squares width:");
+            w = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the squares height:");
+            h = int.Parse(Console.ReadLine());
+            return w;
+        }
+
+        public static void CalcSquareArea()
+        {
+
+        }
+    }
+}
---
+++ Exercises/Exercises/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string menu;
+
+            do
+            {
+                Console.WriteLine("Enter an operation to begin [x, /, +, -] or type 'exit' to quit.");
+
+                menu = Console.ReadLine();
+                switch (menu)
+                {
+                    case "x":
+                        Math.MultiplyNums();
+                        break;
+                    case "/":
+                        Math.DivideNums();
+                        break;
+                    case "+":
+                        Math.AddNums();
+                        break;
+                    case "-":
+                        Math.SubtractNums();
+                        break;
+                    case "exit":
+                        break;
+                    default:
+                        Console.WriteLine("Error. Not an option!");
+                        break;
+
+                }
+            } while (menu != "exit");
+        }
+        
+    }
+
+    class Math
+    {
+        public static void MultiplyNums()
+        {
+            Console.WriteLine(EnterFirst("multiplied"));
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine(EnterSecond("multiplied"));
+            int num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{num1} x {num2} = {num1 * num2}");
+            Console.WriteLine("Press 'Enter' to continue...");
+            Console.ReadLine();
+        }
+
+        public static void DivideNums()
+        {
+            Console.WriteLine(EnterFirst("divided"));
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine(EnterSecond("divided"));
+            int num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{num1} / {num2} = {num1 / num2}");
+            Console.WriteLine("Press 'Enter' to continue...");
+            Console.ReadLine();
+        }
+
+        public static void AddNums()
+        {
+            Console.WriteLine(EnterFirst("added"));
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine(EnterSecond("added"));
+            int num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{num1} + {num2} = {num1 + num2}");
+            Console.WriteLine("Press 'Enter' to continue...");
+            Console.ReadLine();
+        }
+
+        public static void SubtractNums()
+        {
+            Console.WriteLine(EnterFirst("subtracted"));
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine(EnterSecond("subtracted"));
+            int num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{num1} - {num2} = {num1 - num2}");
+            Console.WriteLine("Press 'Enter' to continue...");
+            Console.ReadLine();
+        }
+
+        public static string EnterFirst(string enterMessage)
+        {
+            return $"Enter the first number to be {enterMessage}:";
+        }
+
+        public static string EnterSecond(string enterMessage)
+        {
+            return $"Enter the second number to be {enterMessage}:";
+        }
+    }
+}
